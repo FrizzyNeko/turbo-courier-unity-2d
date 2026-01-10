abdbd67108d6e39165818570dc7f17be5493d9eb
asrin <asrin.senemoglu@gmail.com>
2026-01-10 19:25:22 +0300

    completed Turbo Courier 2D top-down driving game

---
+++ Assets/Scripts/Delivery.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class Delivery : MonoBehaviour
+{
+    bool hasPackage;
+    [SerializeField] float destroyDelay = 0f;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Package") && !hasPackage)
+        {
+            Debug.Log("Picked up package.");
+            hasPackage = true;
+            GetComponent<ParticleSystem>().Play();
+            Destroy(collision.gameObject, destroyDelay);
+        }
+        if (collision.CompareTag("Customer") && hasPackage)
+        {
+            Debug.Log("Package delivered.");
+            hasPackage = false;
+            GetComponent<ParticleSystem>().Stop();
+            Destroy(collision.gameObject, destroyDelay);
+        }
+    }
+}
